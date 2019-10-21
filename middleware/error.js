7294a52dc4b133da3c301f7154a24fc700483465
po40361 <poweitsao@gmail.com>
2019-10-21 17:06:11 -0400

    added middleware for express

---
+++ middleware/error.js
@@ -0,0 +1,9 @@
+const winston = require('winston');
+require('winston-mongodb');
+
+module.exports = function(err, req, res, next){
+    winston.error(err.message, err);
+
+    res.status(500).send('Something went wrong');
+}
+
